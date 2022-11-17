d42b832bab4de10e2feeddfed526454b2b957727
Karan Shah <kshah@zuora.com>
2022-11-18 00:01:47 +0530

    Implemented the dropdown

--- src/components/LineData/LineLoop.js
+++ src/components/LineData/LineLoop.js
@@ -1,4 +1,3 @@
-import LineData from "./LineData";
 import gateways from "../static/DefaultPageData";
 import styles from './LineData.module.css';
 
@@ -6,12 +5,12 @@ const generateLines = (index2) => {
     const content = [];
     let data ;
     for (let index = 0; index < gateways[0].healthMetrics.length; index++) {
-        if (gateways[index2.index].healthMetrics[index].integrationFailure == 0){
-           data = <div className={styles.LineData_G}></div>
+        if (gateways[index2.index].healthMetrics[index].integrationFailure === 0){
+           data = <div className={styles.LineData_G} key = {index}></div>
         } else if (gateways[index2.index].healthMetrics[index].integrationFailure >= 80) {
-            data = <div className={styles.LineData_R}></div>
+            data = <div className={styles.LineData_R} key = {index} ></div>
         } else {
-            data = <div className={styles.LineData_Y}></div>
+            data = <div className={styles.LineData_Y} key = {index} ></div>
         }
         content.push(data);
     }
